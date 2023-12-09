ade78256e72e89485c9fe4837b068b64e5508246
rexdanie <153270087+rexdanie@users.noreply.github.com>
2023-12-09 20:26:35 +0800

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Rex-Daniel-Navarro-Block4-Quiz"
+include(":app")
+ 
